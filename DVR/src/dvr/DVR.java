40b64a2fcc9d2be19bed0f6075fe565f800ef837
AMatejunas <matejunas.alex@gmail.com>
2015-11-04 10:51:07 -0700

    Created part of program flow with place holder values

--- DVR/src/dvr/DVR.java
+++ DVR/src/dvr/DVR.java
@@ -1,21 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dvr;
 
-/**
- *
- * @author alexm
- */
+// Imports
+import java.io.*;
+import java.net.*;
+
 public class DVR {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+           System.out.print("Enter the rounter's ID: ");
+           
+           // Read in router id
+           char id = 'X'; // place holder; my id
+           
+           // Get port number and distance vector for this router and ports of
+           // the other two routers
+           int[] ports = {6666, 6667, 6668}; // place holder; ports in network
+           int me = 0; // place holder; index of my port
+           int[] distVec = {0, 2, 7}; // place holder; my distance vector
+           
+           System.out.printf("Router %c is running on port %d\n", id, ports[me]);
+           System.out.printf("Distance vector on router %c is:\n", id);
     }
     
+    public static void printDistanceVector(int[] vector) {
+        System.out.print("<");
+        //System.out.print(String.join(",", vector));
+    }
 }
